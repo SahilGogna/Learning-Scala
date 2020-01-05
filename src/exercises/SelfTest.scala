c0ef3a5254cec9a61a3cd100bf4d0698bc48d48f
Sahil Gogna <gogna.sahil95@gmail.com>
2020-01-05 17:33:23 -0500

    Implemented list made generic

--- src/exercises/SelfTest.scala
+++ src/exercises/SelfTest.scala
@@ -1,5 +1,7 @@
 package exercises
 
+import java.util.NoSuchElementException
+
 import scala.annotation.tailrec
 
 object SelfTest extends App {
@@ -68,4 +70,95 @@ object SelfTest extends App {
   }
 
   println(fiboTail(8))
+
+// testing list
+
+//  val list = new SLinkedList(1,EmptyList)
+//  println(s"List ${list.head}")
+
+  val list : MyListN[Int] = new SLinkedList(1,new SLinkedList(2,new SLinkedList(3,EmptyList)))
+  val list2 : MyListN[String] = new SLinkedList("hi",new SLinkedList("hello",new SLinkedList("hi",EmptyList)))
+  //  println(s"List ${list.head}")
+  println(list.printList(list))
+  println(list2.printList(list2))
+
+
+
+}
+
+/*
+Counter class
+  - receives an int value
+  - method current count
+  - method to increment/decrement => new Counter
+  - overload inc/dec to receive an amount
+*/
+
+class Counter(val count:Int = 0) {
+  def inc:Counter = {
+    new Counter(count+1)
+  }
+
+  def dec: Counter = {
+    new Counter(count-1)
+  }
+
+  def inc(n:Int):Counter = {
+    if(n <= 0) this
+    else inc.inc(n-1)
+  }
+
+  def dec(n:Int):Counter = {
+    if(n <= 0) this
+    else dec.dec(n-1)
+  }
+}
+
+abstract class MyListN[+A]{
+  /*
+   head = first element of  the  list
+   tail = remainder of the list
+   isEmpty = is this list empty
+   add(int) => new list with this element added
+   toString => a string representation of the list
+ */
+
+  def head:A
+  def tail : MyListN[A]
+  def isEmpty : Boolean
+  def add[B >: A](e:B) : MyListN[B]
+  def printList[B >: A] (list:MyListN[B]) : String
+}
+
+object EmptyList extends MyListN[Nothing] {
+
+  override def head: Nothing = throw new NoSuchElementException
+
+  override def tail: MyListN[Nothing] = throw new NoSuchElementException
+
+  override def isEmpty: Boolean = true
+
+  override def add[B >: Nothing](e: B): MyListN[B] = new SLinkedList(e, EmptyList)
+
+  override def printList[B >: Nothing](list: MyListN[B]): String = " "
+}
+
+class SLinkedList[+A](h:A, t:MyListN[A]) extends MyListN[A] {
+
+  override def head: A = h
+
+  override def tail: MyListN[A] = t
+
+  override def isEmpty: Boolean = false
+
+  override def add[ B >: A](e: B): MyListN[B] = new SLinkedList(e,this)
+
+  override def printList[B >: A](list: MyListN[B]): String = {
+    def printHelper[B >: A] (list: MyListN[B], helper : String):String = {
+      if (list.isEmpty == true) helper
+      else printHelper(list.tail,helper+list.head+" ")
+    }
+    printHelper(list,"")
+  }
+
 }
